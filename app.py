73be6efbe5de4ab882f36778cbc11b7e21acd174
NHS <sweety_luke@hanmail.net>
2022-08-12 15:05:14 +0900

    음식 추천 웹 플라스크

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route('/')    #홈페이지 가장 상위 루트 www.aaa.com/
+def hello():
+    return 'Hello, World!'
+
+@app.route('/naver')    
+def naver():
+    return '네이버 사이트!'
+
+@app.route('/food')    
+def food():
+    foodlist = ["짜장면", "스시", "피자", "치킨", "커리"]
+    foodname = random.choice(foodlist)
+    return render_template("food.html", data=foodname)
+
+if __name__ == '__main__':
+    app.run()
