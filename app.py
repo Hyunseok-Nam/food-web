92d09d803092e3d64e9af5300de7be37f12c3c46
NHS <sweety_luke@hanmail.net>
2022-08-12 16:05:55 +0900

    음식 추천 웹 플라스크

--- app.py
+++ app.py
@@ -5,18 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')    #홈페이지 가장 상위 루트 www.aaa.com/
 def hello():
-    return render_template("index.html")
+    return render_template('index.html')
     # return 'Hello, World!'
 
 @app.route('/naver')    
 def naver():
-    return '네이버 사이트!'
+    return '네이버 사이트'
 
 @app.route('/food')    
 def food():
     foodlist = ["짜장면", "스시", "피자", "치킨", "커리"]
     foodname = random.choice(foodlist)
-    return render_template("food.html", data=foodname)
+    return render_template('food.html', data=foodname)
 
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+    app.run(host="0.0.0.0", port=80)
